824b16d447100e127e39d677f7476d7b28fe2dd1
Junhui Zhang <junhuizhang.g@gmail.com>
2024-06-14 22:47:51 +1200

    1.12-1.14

--- part1-ex/src/App.js
+++ part1-ex/src/App.js
@@ -1,56 +1,33 @@
 import { useState } from 'react'
 
-const Button = ({ onClick, text }) => (
-  <button onClick={onClick}>
-    {text}
-  </button>
-)
-const StatisticLine = ({ text, value }) => {
-  return (
-    <div>
-      {text} {value}
-    </div>
-  )
-}
-const Statistic = (props) => {
-  return (
-    <div>
-      <StatisticLine text="good" value={props.good} />
-      <StatisticLine text="neutral" value={props.neutral} />
-      <StatisticLine text="bad" value={props.bad} />
-      <StatisticLine text="all" value={props.total} />
-      <StatisticLine text="average" value={(props.good - props.bad) / props.total} />
-    </div>
-  )
-}
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [total, setTotal] = useState(0)
-  const handleGood = () => {
-    setGood(good + 1)
-    setTotal(total + 1)
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+  const getRandomIntInclusive = (min, max) => {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // 包含最小值和最大值
   }
-  const handleNeutral = () => {
-    setNeutral(neutral + 1)
-    setTotal(total + 1)
-  }
-  const handleBad = () => {
-    setBad(bad + 1)
-    setTotal(total + 1)
-  }
+
+
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
   return (
     <div>
-      {/* <button onClick={handleGood}>good</button>
-      <button onClick={handleNeutral}>neutral</button>
-      <button onClick={handleBad}>bad</button> */}
-      <Button onClick={handleGood} text="good" />
-      <Button onClick={handleNeutral} text="neutral" />
-      <Button onClick={handleBad} text="bad" />
-      {/* <Statistics good={good} neutral={neutral} bad={bad} total={total} /> */}
-      <Statistic good={good} neutral={neutral} bad={bad} total={total} />
+      <button onClick={() => setVotes(votes.map((vote, index) => index === selected ? vote + 1 : vote))}>vote</button>
+      <button onClick={() => setSelected(getRandomIntInclusive(0, anecdotes.length - 1))}>next anecdote</button>
+      {anecdotes[selected]} has {votes[selected]} votes
+      <h1>most notes</h1>
+      {anecdotes[votes.indexOf(Math.max(...votes))]} has {Math.max(...votes)} votes
     </div>
   )
 }
+
 export default App
